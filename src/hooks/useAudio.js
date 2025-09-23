de3128a1b2a02843e3b2469a52ee77ca039ebac2
nabilBerji <nabil.berji@gmail.com>
2025-09-23 11:24:22 +0100

    fix logo in browser

--- src/hooks/useAudio.js
+++ src/hooks/useAudio.js
@@ -2,15 +2,15 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useAuthContext } from "./useAuthContext";
+import { SERVER_API_URL } from "../constants/serverApi";
 
 export const useAudio = () => {
   const [audios, setAudios] = useState([]);
-  const [loading, setLoading] = useState(true); // true par défaut pour bloquer le dashboard tant que pas chargé
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const { user } = useAuthContext();
 
   useEffect(() => {
-    // si user non disponible, ne pas lancer la requête et ne pas bloquer indéfiniment
     if (!user?.agentId || !user?.token) {
       setLoading(false);
       return;
@@ -23,7 +23,7 @@ export const useAudio = () => {
       setError(null);
       try {
         const response = await axios.get(
-          `http://localhost:4000/api/faq/audios?agentId=${user.agentId}`,
+          `${SERVER_API_URL}/faq/audios?agentId=${user.agentId}`,
           {
             headers: {
               Authorization: `Bearer ${user.token}`,
@@ -34,7 +34,6 @@ export const useAudio = () => {
         setAudios(response.data);
         console.log("in use Audio ", response.data);
       } catch (err) {
-        // gérer annulation
         if (axios.isCancel?.(err) || err.name === "CanceledError") return;
         setError(err.message || "Erreur lors de la récupération des audios");
       } finally {
